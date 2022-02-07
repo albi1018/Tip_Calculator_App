83f326b4e1b8819073037b374da33cc1a8668c25
albi1018 <albi1018@gmail.com>
2022-02-07 22:18:18 +0100

    js

--- tip-calculator-app-main/app.js
+++ tip-calculator-app-main/app.js
@@ -10,16 +10,21 @@ const total = document.getElementById("total");
 
 //EVENTS//
 billInput.addEventListener("input", setBillValue);
-// buttons.addEventListener(setButtonsValue);
-// customInput.addEventListener("input", setCustomValue);
-// peopleInput.addEventListener("input", setPeopleValue);
+customInput.addEventListener("input", setCustomValue);
+peopleInput.addEventListener("input", setPeopleValue);
 //EVENTS//
 
 let billValue = 0.0;
 let buttonValue = 0.0;
+let peopleValue = 0;
 
 function validateBillValue(s) {
     const rgx = /^[0-9]*[\.\,]?[0-9]*$/;
+    return s.match(rgx);
+}
+
+function validateCustomValue(s) {
+    const rgx = /^([1-9][0-9]{0,3}|10000)$/;
     return s.match(rgx);
 }
 
@@ -32,23 +37,66 @@ function setBillValue() {
             billValue = parseFloat(billInput.value.replaceAll(',', '.'))
             console.log(billValue);
         }
+        calculateTip();
     } else {
         billInput.style.border = '1px solid red'
     }
 }
 
 buttons.forEach(btn => {
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', function(e) {
         buttons.forEach(btn => btn.classList.remove('active'));
-        this.classList.add('active');
+        if (this.classList.contains('active')) {
+            this.classList.remove('active');
+            console.log(111);
+        } else {
+            this.classList.add('active');
+            console.log(2222);
+        }
         buttonValue = parseFloat(this.innerHTML)/100;
+        customInput.value = '';
         console.log(buttonValue);
+        calculateTip();
     })
 })
 
+function setCustomValue() {
+    if (validateCustomValue(customInput.value)) {
+        customInput.style.border = '1px solid transparent'
+        buttonValue = parseFloat(customInput.value)/100;
+        buttons.forEach(btn => btn.classList.remove('active'));
+        console.log(buttonValue);
+        calculateTip();
+    } else {
+        customInput.style.border = '1px solid red'
+    }
+}
 
+function setPeopleValue() {
+    if (validateCustomValue(peopleInput.value)) {
+        peopleValue = parseFloat(peopleInput.value);
+        errorMessage.classList.remove('show');
+        peopleInput.style.border = '1px solid transparent';
+        console.log(peopleValue);
+        calculateTip();
+    }
+    else {
+        peopleInput.style.border = '1px solid red';
+        errorMessage.classList.add('show');
+    }
+}
 
-
+function calculateTip() {
+    let amountNumber = billValue * buttonValue / peopleValue;
+    let totalNumber = (billValue * buttonValue + billValue) / peopleValue;
+    if (isNaN(amountNumber)) {
+        console.log('nan');
+        amount.innerHTML = '$0.00'
+    } else {
+        amount.innerHTML = '$' + amountNumber.toFixed(2);
+        total.innerHTML = '$' + totalNumber.toFixed(2);
+    }
+}
 
 
 
