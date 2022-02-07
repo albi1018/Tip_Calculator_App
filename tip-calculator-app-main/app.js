c7d2378c6f695357cb461521de889d209ebd9069
albi1018 <albi1018@gmail.com>
2022-02-07 16:13:47 +0100

    app.js

--- tip-calculator-app-main/app.js
+++ tip-calculator-app-main/app.js
@@ -6,88 +6,45 @@ const peopleInput = document.getElementById('people');
 const errorMessage = document.querySelector(".errorMessage");
 const amount = document.getElementById("amount");
 const total = document.getElementById("total");
-console.log(amount);
-console.log(total);
 //ALL ELEMENTS//
 
 //EVENTS//
 billInput.addEventListener("input", setBillValue);
-customInput.addEventListener("input", setCustomValue);
-peopleInput.addEventListener("input", setPeopleValue);
+// buttons.addEventListener(setButtonsValue);
+// customInput.addEventListener("input", setCustomValue);
+// peopleInput.addEventListener("input", setPeopleValue);
 //EVENTS//
 
 let billValue = 0.0;
-let btnValue = 0.0;
-let peopleNumber = 0;
+let buttonValue = 0.0;
 
-//ACTIVE BUTTONS//
-    buttons.forEach((btn) => {
-        btn.addEventListener("click", function(e) {
-            buttons.forEach(btn=>btn.classList.remove("active"))
-            this.classList.add("active");
-            console.log(btnValue);
-            btnValue = parseFloat(e.target.innerHTML)/100;
-            customInput.value = '';
-            calculate();
-        })
-    })
-//ACTIVE BUTTONS//
-
-function validateFloat(s) {
-    const regex = /^[0-9]*\.?[0-9]*$/;
-    return s.match(regex)
-}
-
-function validateInt(s) {
-    const regex = /^([1-9][0-9]{0,3}|10000)$/;
-    return s.match(regex);
+function validateBillValue(s) {
+    const rgx = /^[0-9]*[\.\,]?[0-9]*$/;
+    return s.match(rgx);
 }
 
 function setBillValue() {
-    if (billInput.value.includes(',')) {
-        billInput.value.replace(',','.')
-    }
-
-    if (validateFloat(billInput.value)) {
-        billInput.style.border = '1px solid green';
-    } else {
-        billInput.style.border = '1px solid red';
-    }
-    billValue = parseFloat(billInput.value);
-    calculate();
-}
-
-function setCustomValue() {
-    if (validateFloat(customInput.value)) {
-        buttons.forEach(btn=>btn.classList.remove("active"));
-        customInput.style.border = '1px solid green';
-        billValue = parseFloat(customInput.value/100);
+    if (validateBillValue(billInput.value)) {
+        billValue = parseFloat(billInput.value);
+        billInput.style.border = '1px solid transparent'
         console.log(billValue);
-        calculate();
-    } else {
-        customInput.style.border = '1px solid red';
-    }
-}
-
-function setPeopleValue() {
-    if (!validateInt(peopleInput.value) || peopleInput.value <= 0) {
-        errorMessage.classList.add('show');
-        peopleInput.style.border = '1px solid red';
-    } else {
-        errorMessage.classList.remove('show');
-        peopleInput.style.border = '';
-        peopleNumber = peopleInput.value;
-    }
-    calculate();
-}
-
-function calculate() {
-        if (peopleNumber >=1) {
-            let tipAmount = billValue * btnValue / peopleNumber;
-            // console.log(tipAmount);
-            amount.innerHTML = '$' + tipAmount.toFixed(2);
+        if (billInput.value.includes(',')) {
+            billValue = parseFloat(billInput.value.replaceAll(',', '.'))
+            console.log(billValue);
         }
+    } else {
+        billInput.style.border = '1px solid red'
+    }
 }
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        buttonValue = parseFloat(this.innerHTML)/100;
+        console.log(buttonValue);
+    })
+})
 
 
 
